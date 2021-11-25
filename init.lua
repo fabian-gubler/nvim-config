497dc2f912fabb4bef058fb62ceebdd42b63ff06
fabian-gubler <fabian.gubler@hotmail.com>
2021-11-25 13:08:18 +0100

    update

--- init.lua
+++ init.lua
@@ -1,4 +1,11 @@
 local vim = vim
+local fn = vim.fn
+
+-- Bootstrap packer
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+end
 
 -- Space as leader key
 vim.api.nvim_set_keymap('', '<Space>', '<Nop>', { noremap = true, silent = true })
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,12 +1,5 @@
 local vim = vim
 
--- Bootstrap packer
-local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
-
-if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-  vim.fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
-end
-
 return require('packer').startup(function(use)
 -- Packer can manage itself
 	use 'wbthomason/packer.nvim'
