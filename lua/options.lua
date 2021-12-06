3c4f27f2d4e4b8e0c96e988644d393842d4fc7c5
fabian-gubler <fabian.gubler@hotmail.com>
2021-12-06 12:00:01 +0100

    update

--- lua/options.lua
+++ lua/options.lua
@@ -39,8 +39,8 @@ cmd 'au CmdlineEnter /,? :set hlsearch'
 cmd 'au CmdlineLeave /,? :set nohlsearch'
 
 cmd 'autocmd FileType typescript setlocal ts=2 sts=2 sw=2'
-cmd 'autocmd FileType javascript, html setlocal ts=2 sts=2 sw=2'
 cmd 'autocmd FileType html setlocal ts=2 sts=2 sw=2'
+cmd 'autocmd FileType javascript setlocal ts=2 sts=2 sw=2'
 cmd 'autocmd FileType lua setlocal ts=3 sts=3 sw=3'
 
 -- Set barbar's options
