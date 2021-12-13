1bf33916b6f424634622ef74c180ebb77f4a20a3
fabian-gubler <fabian.gubler@hotmail.com>
2021-12-13 11:00:01 +0100

    automated update

--- lua/options.lua
+++ lua/options.lua
@@ -42,6 +42,7 @@ cmd 'autocmd FileType typescript setlocal ts=2 sts=2 sw=2'
 cmd 'autocmd FileType html setlocal ts=2 sts=2 sw=2'
 cmd 'autocmd FileType javascript setlocal ts=2 sts=2 sw=2'
 cmd 'autocmd FileType lua setlocal ts=3 sts=3 sw=3'
+cmd 'au BufEnter,BufNew *.gss set filetype=python'
 
 -- Set barbar's options
 require'core.bar'
