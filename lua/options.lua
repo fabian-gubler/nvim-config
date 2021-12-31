bd9e16d8fd8708c23195145f0fbef61cd7091f65
fabian-gubler <fabian.gubler@hotmail.com>
2021-12-31 03:00:01 +0100

    automated update

--- lua/options.lua
+++ lua/options.lua
@@ -1,6 +1,6 @@
 local o = vim.opt
 local g = vim.g
-local home = os.getenv( "HOME" )
+local home = os.getenv("HOME")
 local cmd = vim.cmd
 local indent = 4
 
@@ -8,8 +8,8 @@ local indent = 4
 g.nvim_tree_quit_on_open = 1
 g.auto_save = 1
 g.auto_save_silent = 1
-g.EasyMotion_keys='arstdhneioqwfpluygmbjzxcvk1234567890,'
-g.nord_borders = 'true'
+g.EasyMotion_keys = "arstdhneioqwfpluygmbjzxcvk1234567890,"
+g.nord_borders = "true"
 
 -- Options
 o.termguicolors = true
@@ -23,7 +23,7 @@ o.splitright = true
 o.ignorecase = true
 o.smartcase = true
 o.undofile = true
-o.undodir = home .. '/.local/share/nvim/undodir'
+o.undodir = home .. "/.local/share/nvim/undodir"
 o.clipboard = "unnamedplus"
 o.smartindent = true
 o.signcolumn = "yes:1"
@@ -35,17 +35,17 @@ o.softtabstop = indent
 o.expandtab = false
 
 -- Autocommands
-cmd 'au VimEnter * :set showtabline=0'
-cmd 'au BufNew,BufRead * setl fo-=orc'
-cmd 'au CmdlineEnter /,? :set hlsearch'
-cmd 'au CmdlineLeave /,? :set nohlsearch'
+cmd("au VimEnter * :set showtabline=0")
+cmd("au BufNew,BufRead * setl fo-=orc")
+cmd("au CmdlineEnter /,? :set hlsearch")
+cmd("au CmdlineLeave /,? :set nohlsearch")
 
-cmd 'autocmd FileType typescript setlocal ts=2 sts=2 sw=2'
-cmd 'autocmd FileType typescriptreact setlocal ts=2 sts=2 sw=2'
-cmd 'autocmd FileType html setlocal ts=2 sts=2 sw=2'
-cmd 'autocmd FileType javascript setlocal ts=2 sts=2 sw=2'
-cmd 'autocmd FileType lua setlocal ts=3 sts=3 sw=3'
-cmd 'au BufEnter,BufNew *.gss set syntax=cpp'
+cmd("autocmd FileType typescript setlocal ts=2 sts=2 sw=2")
+cmd("autocmd FileType typescriptreact setlocal ts=2 sts=2 sw=2")
+cmd("autocmd FileType html setlocal ts=2 sts=2 sw=2")
+cmd("autocmd FileType javascript setlocal ts=2 sts=2 sw=2")
+cmd("autocmd FileType lua setlocal ts=3 sts=3 sw=3")
+cmd("au BufEnter,BufNew *.gss set syntax=cpp")
 
 -- Set barbar's options
-require'core.bar'
+require("core.bar")
