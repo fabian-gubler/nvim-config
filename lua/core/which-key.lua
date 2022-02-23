bd9e5205798e11ce6870cd8df68a41ce4fa25427
fabian-gubler <fabian.gubler@hotmail.com>
2022-02-23 14:00:01 +0100

    automated update

--- lua/core/which-key.lua
+++ lua/core/which-key.lua
@@ -35,6 +35,7 @@ wk.register({
 		name = "Vimtex",
 		c = { "<cmd>VimtexCompile<cr>", "Compile"},
 		s = { "<cmd>VimtexStop<cr>", "Stop"},
+		S = { "<cmd>VimtexStatus<cr>", "Status"},
 		C = { "<cmd>VimtexClean<cr>", "Clean"},
 		t = { "<cmd>VimtexTocToggle<cr>", "Table of Contents"},
   },
