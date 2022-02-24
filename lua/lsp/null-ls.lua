cd526fe6a38aecefc4bc90fc49d68bbecfda9175
fabian-gubler <fabian.gubler@hotmail.com>
2022-02-24 20:00:01 +0100

    automated update

--- lua/lsp/null-ls.lua
+++ lua/lsp/null-ls.lua
@@ -6,14 +6,14 @@ end
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
 local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
--- local diagnostics = null_ls.builtins.diagnostics
+local diagnostics = null_ls.builtins.diagnostics
 
 null_ls.setup({
 	debug = false,
 	sources = {
 		formatting.prettier.with({ extra_args = { "--single-quote", "--jsx-single-quote", "--trailingComma none" } }),
 		formatting.stylua,
-		-- formatting.black.with({ extra_args = { "--fast" } }),
-		-- diagnostics.flake8
+		formatting.black.with({ extra_args = { "--fast" } }),
+		diagnostics.flake8
 	},
 })
