59ae94fa2d226b8e3f53f0f8aad307a210eebcec
Fabian Gubler <fabian.gubler@hotmail.com>
2022-10-08 18:01:46 +0200

    remove tab coloring

--- init.lua
+++ init.lua
@@ -12,18 +12,6 @@ require('lsp')
 -- Set Colorsceme
 vim.cmd "colorscheme nord"
 
--- Tab Colours
-vim.cmd "hi BufferTabpageFill guibg=#2E3440"
-
-vim.cmd "hi BufferCurrent guifg=#ECEFF4 guibg=#3b4252"
-vim.cmd "hi BufferCurrentSign guibg=#3b4252"
-
-vim.cmd "hi BufferInactive guifg=#7D869A guibg=#2e3440"
-vim.cmd "hi BufferInactiveSign guibg=#2e3440"
-
-vim.cmd "hi BufferVisible guibg=#2E3440 guifg=#7D869A"
-vim.cmd "hi BufferVisibleSign guibg=#2E3440"
-
 -- Highlight on yank
 vim.api.nvim_exec(
   [[
