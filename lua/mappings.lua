113945273caab8b7702cc5d31d339bc88c17fd82
Fabian Gubler <fabian.gubler@hotmail.com>
2023-01-04 16:28:52 +0100

    manual update

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -19,6 +19,7 @@ keymap("n", "<leader><leader>", ":Telescope find_files<CR>", opts)
 keymap("n", "<leader>e", ":NvimTreeToggle<CR>")
 keymap("n", "<leader>E", ":NvimTreeFindFileToggle<CR>")
 keymap("n", "<leader>o", ":b#<CR>")
+keymap("n", "<leader>u", ":UndotreeToggle<CR>")
 
 -- copilot
 keymap("n", "<leader>c", function() require("copilot.suggestion").toggle_auto_trigger() end, opts)
