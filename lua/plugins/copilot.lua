1df55a540a2b9e6340714467d97779b2d23f4609
Fabian Gubler <fabian.gubler@hotmail.com>
2023-03-26 16:07:28 +0200

    automated update

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -21,7 +21,7 @@ return {
 			},
 			suggestion = {
 				enabled = true,
-				auto_trigger = false,
+				auto_trigger = true,
 				debounce = 75,
 				keymap = {
 					accept = "<M-l>",
@@ -41,7 +41,7 @@ return {
 				hgcommit = false,
 				svn = false,
 				cvs = false,
-				["."] = false,
+				-- ["."] = false,
 			},
 			copilot_node_command = "node", -- Node.js version must be > 16.x
 			server_opts_overrides = {},
