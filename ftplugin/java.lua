8dfe1a620fcf590a9c01c125071e6addb1a64dda
Fabian Gubler <fabian.gubler@protonmail.com>
2023-06-14 13:59:59 +0200

    manual update

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -160,10 +160,19 @@ local config = {
 
 require("jdtls").start_or_attach(config)
 
-vim.cmd("command! -buffer -nargs=? -complete=custom,v:lua.require'jdtls'._complete_compile JdtCompile lua require('jdtls').compile(<f-args>)")
-vim.cmd("command! -buffer -nargs=? -complete=custom,v:lua.require'jdtls'._complete_set_runtime JdtSetRuntime lua require('jdtls').set_runtime(<f-args>)")
-vim.cmd("command! -buffer JdtUpdateConfig lua require('jdtls').update_project_config()")
-vim.cmd("command! -buffer JdtBytecode lua require('jdtls').javap()")
+-- vim.cmd("command! -buffer -nargs=? -complete=custom,v:lua.require'jdtls'._complete_compile JdtCompile lua require('jdtls').compile(<f-args>)")
+-- vim.cmd("command! -buffer -nargs=? -complete=custom,v:lua.require'jdtls'._complete_set_runtime JdtSetRuntime lua require('jdtls').set_runtime(<f-args>)")
+-- vim.cmd("command! -buffer JdtUpdateConfig lua require('jdtls').update_project_config()")
+-- vim.cmd("command! -buffer JdtBytecode lua require('jdtls').javap()")
+
+vim.cmd [[
+command! -buffer -nargs=? -complete=custom,v:lua.require'jdtls'._complete_compile JdtCompile lua require('jdtls').compile(<f-args>)
+command! -buffer -nargs=? -complete=custom,v:lua.require'jdtls'._complete_set_runtime JdtSetRuntime lua require('jdtls').set_runtime(<f-args>)
+command! -buffer JdtUpdateConfig lua require('jdtls').update_project_config()
+command! -buffer JdtJol lua require('jdtls').jol()
+command! -buffer JdtBytecode lua require('jdtls').javap()
+command! -buffer JdtJshell lua require('jdtls').jshell()
+]]
 
 local keymap = vim.keymap.set
 local opts = { silent = true }
