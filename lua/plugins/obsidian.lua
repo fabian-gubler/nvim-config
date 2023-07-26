13d60e1aa05f77372f81420ce52cb7c908312a3a
Fabian Gubler <fabian.gubler@hotmail.com>
2023-07-26 14:42:06 +0200

    automated update

---
+++ lua/plugins/obsidian.lua
@@ -0,0 +1,28 @@
+return {
+	"epwalsh/obsidian.nvim",
+	lazy = true,
+	event = { "BufReadPre /data/nextcloud/obsidian/**.md" },
+	-- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand':
+	dependencies = {
+		-- Required.
+		"nvim-lua/plenary.nvim",
+	},
+	opts = {
+		dir = "/data/nextcloud/obsidian", -- no need to call 'vim.fn.expand' here
+
+		-- see below for full list of options 👇
+	},
+	config = function(_, opts)
+		require("obsidian").setup(opts)
+
+		-- Optional, override the 'gf' keymap to utilize Obsidian's search functionality.
+		-- see also: 'follow_url_func' config option below.
+		vim.keymap.set("n", "gf", function()
+			if require("obsidian").util.cursor_on_markdown_link() then
+				return "<cmd>ObsidianFollowLink<CR>"
+			else
+				return "gf"
+			end
+		end, { noremap = false, expr = true })
+	end,
+}
