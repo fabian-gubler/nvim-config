90ad536efb019600607d0a7fa86740d0be82d93a
Fabian Gubler <fabian.gubler@protonmail.com>
2023-12-11 21:29:25 +0100

    add useful telescope keymaps

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,11 +2,11 @@ return {
 	"nvim-telescope/telescope.nvim",
 	keys = {
 		{ "<leader>g", ":Telescope live_grep<CR>", desc = "telescope grep" },
-		{
-			"<leader><leader>",
-			":Telescope find_files hidden=true no_ignore=true<CR>",
-			desc = "telescope find files",
-		},
+		{ "<leader><leader>", ":Telescope find_files hidden=true no_ignore=true<CR>",
+		desc = "telescope find files", },
+		{ "<leader>th", ":Telescope help_tags<cr>"},
+		{ "<leader>tk", ":Telescope keymaps<cr>"},
+		-- { "<leader>td", ":Telescope diagnostics<cr>"},
 	},
 	config = function()
 		require("telescope").setup({
