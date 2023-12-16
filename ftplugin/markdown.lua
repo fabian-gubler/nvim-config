af5f5e3353594db6ae8823cfcdf474facf89f9bd
Fabian Gubler <fabian.gubler@hotmail.com>
2023-12-16 18:42:07 +0100

    refactor: remove unused files

--- ftplugin/markdown.lua
+++
@@ -1,7 +0,0 @@
-local opts = { noremap = true, silent = true }
-
--- Markdown
-vim.keymap.set("n", "]#", ":keeppatterns s/^/#/e<cr>", opts)
-vim.keymap.set("n", "[#", ":keeppatterns s/^#//e<cr>", opts)
-vim.keymap.set("n", "]#", ":<c-u>keeppatterns '<,'>g/^#/keeppatterns s/^#//<cr>", opts)
-vim.keymap.set("n", "[#", ":<c-u>keeppatterns '<,'>g/^#/keeppatterns s/^#/##/<cr>", opts)
