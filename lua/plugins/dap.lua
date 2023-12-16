7e4e51c30221970db42b78bb9a50e9cb4bc73dff
Fabian Gubler <fabian.gubler@hotmail.com>
2023-12-16 23:31:02 +0100

    refactor: simplify dapui settings, add keymaps

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -8,71 +8,41 @@ return {
 					"DapBreakpoint",
 					{ text = "", texthl = "DiagnosticSignError", linehl = "", numhl = "" }
 				)
+
+				-- Configure dapui layout
 				require("dapui").setup({
-					expand_lines = true,
 					icons = { expanded = "", collapsed = "", circular = "" },
-					mappings = {
-						-- Use a table to apply multiple mappings
-						expand = { "<CR>", "<2-LeftMouse>" },
-						open = "o",
-						remove = "d",
-						edit = "e",
-						repl = "r",
-						toggle = "t",
-					},
-					layouts = {
-						{
-							elements = {
-								{ id = "scopes", size = 0.33 },
-								{ id = "breakpoints", size = 0.17 },
-								{ id = "stacks", size = 0.25 },
-								{ id = "watches", size = 0.25 },
-							},
-							size = 0.33,
-							position = "right",
-						},
-						{
-							elements = {
-								{ id = "repl", size = 0.45 },
-								{ id = "console", size = 0.55 },
-							},
-							size = 0.27,
-							position = "bottom",
-						},
-					},
-					floating = {
-						max_height = 0.9,
-						max_width = 0.5, -- Floats will be treated as percentage of your screen.
-						border = vim.g.border_chars, -- Border style. Can be 'single', 'double' or 'rounded'
-						mappings = {
-							close = { "q", "<Esc>" },
-						},
-					},
 				})
 
+				-- Automatically load / close dapui
 				require("dap").listeners.after.event_initialized["dapui_config"] = function()
 					require("dapui").open()
+				end
+
+				require("dap").listeners.before.event_exited["dapui_config"] = function()
+					require("dapui").close()
 				end
 			end,
 		},
 		{
 			"mfussenegger/nvim-dap-python",
-			config = function()
-				require("dap-python").setup(".venv/bin/python")
-			end,
+			config = function() require("dap-python").setup(".venv/bin/python") end,
 		},
 	},
 	keys = {
-		{ "<leader>db", function() require'dap'.toggle_breakpoint() end, desc = "debug breakpoint" },
-		{ "<leader>dc", function() require'dap'.continue() end, desc = "debug continue" },
-		{ "<leader>do", function() require'dap'.step_over() end, desc = "debug over" },
-		{ "<leader>dO", function() require'dap'.step_out() end, desc = "debug out" },
-		{ "<leader>di", function() require'dap'.step_into() end, desc = "debug into" },
-		{ "<leader>dr", function() require'dap'.repl_open() end, desc = "debug repl" },
-		{ "<leader>du", function() require'dapui'.toggle() end, desc = "debug toggle" },
-		{ "<leader>dl", function() require("telescope").extensions.dap.list_breakpoints() end, desc = "list breakpoints"},
-		{ "<leader>dL", function() require'dap'.clear_breakpoints() end, desc = "debug toggle" },
-		{ "<leader>dd", function() require("telescope").extensions.dap.commands() end, desc = "list commands"},
-		-- { "<leader>dl", "<cmd>lua require'dap'.run_last()<CR>", descr = "debug last" },
+		{ "<leader>dt", function() require("dap").toggle_breakpoint() end },
+		{ "<leader>db", function() require("dap").step_back() end },
+		{ "<leader>dc", function() require("dap").continue() end },
+		{ "<leader>dC", function() require("dap").run_to_cursor() end },
+		{ "<leader>dd", function() require("dap").disconnect() end },
+		{ "<leader>dq", function() require("dap").close() end },
+		{ "<leader>di", function() require("dap").step_into() end },
+		{ "<leader>do", function() require("dap").step_over() end },
+		{ "<leader>du", function() require("dap").step_out() end },
+		{ "<leader>dp", function() require("dap").pause() end },
+		{ "<leader>dp", function() require("dap").pause() end },
+		{ "<leader>dr", function() require("dap").repl_open() end },
+		{ "<leader>dU", function() require("dapui").toggle({ reset = true }) end },
+		{ "<leader>dl", function() require("dap").clear_breakpoints() end },
 	},
 }
