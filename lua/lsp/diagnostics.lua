dc1e3a7159b79931966b64fb649a2e667304caf8
Fabian Gubler <fabian.gubler@hotmail.com>
2023-12-17 14:07:25 +0100

    refactor: extract diagnostics into separate file

---
+++ lua/lsp/diagnostics.lua
@@ -0,0 +1,37 @@
+-- Diagnostics configuration
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+local icons = require("icons")
+local signs = {
+	{ name = "DiagnosticSignError", text = icons.diagnostics.Error },
+	{ name = "DiagnosticSignWarn", text = icons.diagnostics.Warning },
+	{ name = "DiagnosticSignHint", text = icons.diagnostics.Hint },
+	{ name = "DiagnosticSignInfo", text = icons.diagnostics.Information },
+}
+
+for _, sign in ipairs(signs) do
+	vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
+end
+
+local default_diagnostic_config = {
+	virtual_text = false,
+	signs = true,
+	update_in_insert = false,
+	underline = true,
+	severity_sort = true,
+	float = {
+		focusable = true,
+		style = "minimal",
+		border = "rounded",
+		source = "always",
+		header = "",
+		prefix = "",
+	},
+}
+
+vim.diagnostic.config(default_diagnostic_config)
+
+-- Diagnostic keymaps
+vim.keymap.set("n", "<leader>u", vim.diagnostic.open_float)
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist)
--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -1,5 +1,5 @@
--- require "lsp.lsp-installer"
--- require("lsp.handlers").setup()
+-- Load external modules
+require("lsp.diagnostics")
 
 -- Setup language servers.
 -- See `:help lspconfig-setup`
@@ -25,43 +25,6 @@ for _, lsp in ipairs(servers) do
 	lspconfig[lsp].setup(opts)
 end
 
--- Diagnostics configuration
--- See `:help vim.diagnostic.*` for documentation on any of the below functions
-local icons = require("icons")
-local signs = {
-	{ name = "DiagnosticSignError", text = icons.diagnostics.Error },
-	{ name = "DiagnosticSignWarn", text = icons.diagnostics.Warning },
-	{ name = "DiagnosticSignHint", text = icons.diagnostics.Hint },
-	{ name = "DiagnosticSignInfo", text = icons.diagnostics.Information },
-}
-
-for _, sign in ipairs(signs) do
-	vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
-end
-
-local default_diagnostic_config = {
-	virtual_text = false,
-	signs = true,
-	update_in_insert = false,
-	underline = true,
-	severity_sort = true,
-	float = {
-		focusable = true,
-		style = "minimal",
-		border = "rounded",
-		source = "always",
-		header = "",
-		prefix = "",
-	},
-}
-
-vim.diagnostic.config(default_diagnostic_config)
-
--- Diagnostic keymaps
-vim.keymap.set("n", "<leader>u", vim.diagnostic.open_float)
-vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
-vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
-vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist)
 
 -- Use LspAttach autocommand to only map the following keys
 -- after the language server attaches to the current buffer
@@ -72,11 +35,10 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		-- See `:help vim.lsp.*` for documentation on any of the below functions
 		local opts = { buffer = ev.buf }
 
-		vim.keymap.set("n", "gd", require("telescope.builtin").lsp_definitions, opts) -- vim.lsp.buf.definition
-		vim.keymap.set("n", "gi", require("telescope.builtin").lsp_implementations, opts) -- vim.lsp.buf.implementation
-		vim.keymap.set("n", "gr", require("telescope.builtin").lsp_references, opts) -- vim.lsp.buf.references
-		vim.keymap.set("n", "<leader>D", require("telescope.builtin").lsp_type_definitions, opts) -- vim.lsp.buf.type_definition
+		vim.keymap.set("n", "gi", require("telescope.builtin").lsp_implementations, opts)
+		vim.keymap.set("n", "gr", require("telescope.builtin").lsp_references, opts)
 
+		vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
 		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
 		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
 		vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
