45dc3c40d0c9344da363bb8dffa997afe5f98693
Fabian Gubler <fabian.gubler@hotmail.com>
2023-12-17 14:25:24 +0100

    refactor: simplify plugin declaration

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,40 +1,18 @@
-return {
+-- Function for simple plugin setup
+local function setup(plugin_name)
+	return function() require(plugin_name).setup() end
+end
 
-	-- simple setup
+return {
+	-- one liners
 	"shaunsingh/nord.nvim",
 	"907th/vim-auto-save",
 	"stevearc/dressing.nvim",
 	"neovim/nvim-lspconfig",
 	"preservim/vim-markdown",
 
-	{
-		"numToStr/Comment.nvim",
-		config = function()
-			require("Comment").setup()
-		end,
-	},
-
-	{
-		"kylechui/nvim-surround",
-		version = "*", -- Use for stability; omit to use `main` branch for the latest features
-		event = "VeryLazy",
-		config = function()
-			require("nvim-surround").setup({
-				-- Configuration here, or leave empty to use defaults
-			})
-		end,
-	},
-
-	{
-		"lewis6991/gitsigns.nvim",
-		config = function()
-			require("gitsigns").setup()
-		end,
-	},
-	{
-		"windwp/nvim-autopairs",
-		config = function()
-			require("nvim-autopairs").setup()
-		end,
-	},
+	-- simple setup
+	{ "numToStr/Comment.nvim", config = setup("Comment") },
+	{ "lewis6991/gitsigns.nvim", config = setup("gitsigns") },
+	{ "windwp/nvim-autopairs", config = setup("nvim-autopairs") },
 }
