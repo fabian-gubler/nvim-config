082cadc8520604ed0b8cb012da435bcca8b4738e
Fabian Gubler <fabian.gubler@hotmail.com>
2024-01-01 11:59:27 +0100

    feat(harpoon): add harpoon2 as comments

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -12,3 +12,25 @@ return {
 	},
 
 }
+
+-- TODO: Switch to this when quick menu bug is fixed
+
+-- return {
+-- 	"ThePrimeagen/harpoon",
+-- 	branch = "harpoon2",
+-- 	dependencies = "nvim-lua/plenary.nvim",
+-- 	config = function()
+-- 		local harpoon = require("harpoon")
+--
+-- 		harpoon:setup()
+--
+-- 		vim.keymap.set("n", "<A-m>", function() harpoon:list():append() end)
+-- 		vim.keymap.set("n", "<C-h>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+--
+-- 		vim.keymap.set("n", "<A-n>", function() harpoon:list():select(1) end)
+-- 		vim.keymap.set("n", "<A-e>", function() harpoon:list():select(2) end)
+-- 		vim.keymap.set("n", "<A-i>", function() harpoon:list():select(3) end)
+-- 		vim.keymap.set("n", "<A-o>", function() harpoon:list():select(4) end)
+-- 		vim.keymap.set("n", "<A-h>", function() harpoon:list():select(5) end)
+--       end,
+--    }
