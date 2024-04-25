fd5537179e7d26407c0d7088009b298e52551c44
Fabian Gubler <fabian.gubler@hotmail.com>
2024-04-25 17:12:03 +0700

    feat(plugin): add trouble

---
+++ lua/plugins/trouble.lua
@@ -0,0 +1,8 @@
+return {
+	"folke/trouble.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	opts = {
+		severity = nil, -- nil (ALL) or vim.diagnostic.severity.ERROR | WARN | INFO | HINT
+      -- severity = vim.diagnostic.severity.ERROR,
+	},
+}
