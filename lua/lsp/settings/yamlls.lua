b5fa2cc89749079b6a402d307c0dfac02a39c803
Fabian Gubler <fabian.gubler@hotmail.com>
2024-09-10 09:30:11 +0200

    standardized yaml schema prefix

--- lua/lsp/settings/yamlls.lua
+++ lua/lsp/settings/yamlls.lua
@@ -2,7 +2,7 @@ return {
 	settings = {
 		yaml = {
 			schemas = {
-				kubernetes = "k8s-*.yaml",
+				kubernetes = "*.yaml",
 				["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
 				["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
 				["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/**/*.{yml,yaml}",
